41b1eccd0e5298ad9d67fea4249a24255292dead
Vpetros <rawkudodes@hotmail.com>
2025-10-31 22:12:37 +0200

    chore: working on api.products.ts

--- src/services/api.products.ts
+++ src/services/api.products.ts
@@ -1,11 +1,11 @@
-import {z} from zod;
-
-const API_URL= import.meta.env.VITE_API_URL;
-const tenantId= import.meta.env.VITE_TENANT_ID;
-
-export const = z.object({
-    id: z.coerce.number().int(),
-    name: z.string().min(1, {error: "Required"}),
-    slug: z.string().min(1, {error: "Required"}).regex(/^[a-z0-9]+$/, {error: "Only latin letters and numbers"}),
-})
+// import {z} from zod;
+//
+// const API_URL= import.meta.env.VITE_API_URL;
+// const tenantId= import.meta.env.VITE_TENANT_ID;
+//
+// export const = z.object({
+//     id: z.coerce.number().int(),
+//     name: z.string().min(1, {error: "Required"}),
+//     slug: z.string().min(1, {error: "Required"}).regex(/^[a-z0-9]+$/, {error: "Only latin letters and numbers"}),
+// })
 
